a433561c1c9a17388e1874780f37987a6e9093c5
Sasidhar Abburi <111676526+sasi407@users.noreply.github.com>
2022-10-30 19:43:20 +0530

    z=int(input()) c=0 a=[] f=False for i in range(1,(z//2)+1):     for j in range(1,i+1):         if i%j==0:             c+=1     if c==2:         a.append(i)     c=0 for k in a:     for m in a:         if k*m==z:             s=min(k,m)             p=max(k,m)             print(s,p)             f=True             break     if f==True:         break if f==False:     print(-1)

---
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -0,0 +1,23 @@
+z=int(input())
+c=0
+a=[]
+f=False
+for i in range(1,(z//2)+1):
+    for j in range(1,i+1):
+        if i%j==0:
+            c+=1
+    if c==2:
+        a.append(i)
+    c=0
+for k in a:
+    for m in a:
+        if k*m==z:
+            s=min(k,m)
+            p=max(k,m)
+            print(s,p)
+            f=True
+            break
+    if f==True:
+        break
+if f==False:
+    print(-1)
